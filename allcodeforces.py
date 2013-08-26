a6b37755a63493e963483a7306d7b5cc88e0bf1f
Bianca <biancamsoe@gmail.com>
2013-08-26 20:11:23 -0300

    cf

--- allcodeforces.py
+++ allcodeforces.py
@@ -2,16 +2,11 @@ import urllib2
 import re
 
 for i in range(1):
-	contests_html = urllib2.urlopen("http://codeforces.com/contests/page/" + str(i+1)).read()
-	skip_regex = r'Contest history'
-	contest_regex = r'data-contestId="(\d+)".*?<td>[\n|\r| ]*(.*?)<br/>(.*?)'#nbsp;x(\d+)'
-
+	html = urllib2.urlopen("http://codeforces.com/contests/page/" + str(i+1)).read()
+	
+	contests_html = re.search(r'Contest history(.*)', html, re.UNICODE | re.DOTALL)
+	contest_regex = r'data-contestId="(\d+)".*?<td>[\n|\r| ]*(.*?)<br/>.*?nbsp;x(\d+)'
 	contest_pattern = re.compile(contest_regex, re.UNICODE | re.DOTALL)
-	skip = 2
-	for contest_match in contest_pattern.finditer(contests_html):
-		if skip > 2 :
+	for contest_match in contest_pattern.finditer(contests_html.group(1)):
 			print "%s %s %s" % (contest_match.group(1), contest_match.group(2), contest_match.group(3))
-		skip += 1
-
-
-
+			"http://codeforces.com/contest/%s" % (contest_match.group(1))
