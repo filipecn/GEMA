3090382cbde1258693b76381cc8fd984b7772dc1
6878762 <USUARIOS\6878762@lab3-03.grad.icmc.usp.br>
2013-08-27 11:57:35 -0300

    cf

--- allcodeforces.py
+++ allcodeforces.py
@@ -7,6 +7,16 @@ for i in range(1):
 	contests_html = re.search(r'Contest history(.*)', html, re.UNICODE | re.DOTALL)
 	contest_regex = r'data-contestId="(\d+)".*?<td>[\n|\r| ]*(.*?)<br/>.*?nbsp;x(\d+)'
 	contest_pattern = re.compile(contest_regex, re.UNICODE | re.DOTALL)
+	
 	for contest_match in contest_pattern.finditer(contests_html.group(1)):
-			print "%s %s %s" % (contest_match.group(1), contest_match.group(2), contest_match.group(3))
-			"http://codeforces.com/contest/%s" % (contest_match.group(1))
+		print "%s %s %s" % (contest_match.group(1), contest_match.group(2), contest_match.group(3))
+		
+		contest_html = urllib2.urlopen("http://codeforces.com/contest/%s" % (contest_match.group(1))).read()
+		problem_regex = r"/contest/"+re.escape(contest_match.group(1))+r"/problem/(.).*?problem.*?>(.*?)<.*?nbsp;(x\d+|.)"
+		problem_pattern = re.compile(problem_regex, re.UNICODE | re.DOTALL)
+		
+		for problem_match in problem_pattern.finditer(contest_html):
+			print "%s %s %s" % (problem_match.group(1), problem_match.group(2), problem_match.group(3))
+			
+			problem_html = urllib2.urlopen("http://codeforces.com/%s/%s" % (contest_match.group(1),problem_match.group(1))).read()
+			regex = r"Problem tags.*?<span"
