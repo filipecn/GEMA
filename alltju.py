8d969c5f53e1a65e27de14676345fd1deb1eb503
filipecn <fuiripecn@gmail.com>
2013-08-27 16:30:38 -0300

    Update alltju.py

--- alltju.py
+++ alltju.py
@@ -1,7 +1,9 @@
 import urllib2
 import re
 
-for i in range(5):
+from gemadb import Problem
+
+for i in range(1):
 	html = urllib2.urlopen("http://acm.tju.edu.cn/toj/list%s.html" % str(i+1)).read()
 	
 	problem_regex = r'p\([0-9]+,[0-9]+,([0-9]+),"(.*?)",[0-9]+,([0-9]+),([0-9]+),'
@@ -9,3 +11,8 @@ for i in range(5):
 	
 	for problem_match in problem_pattern.finditer(html):
 		print "%s %s %s %s" % (problem_match.group(1), problem_match.group(2), problem_match.group(3), problem_match.group(4))
+		p = Problem()
+		p.judge = "tju"
+		p.name = "%s" % problem_match.group(2)
+		p.judge_id = "%s" % problem_match.group(1)
+		p.judge_difficulty = "0"
