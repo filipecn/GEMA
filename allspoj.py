973e6c471efc3ccf8c479ea4052749e5358694f5
6878762 <USUARIOS\6878762@lab3-03.grad.icmc.usp.br>
2013-08-29 16:28:43 -0300

    spojs completos

--- allspoj.py
+++ allspoj.py
@@ -3,20 +3,34 @@ import re
 
 from gemadb import Problem
 
-for i in range(3):
-	url =  "http://www.spoj.com/problems/classical/sort=0,start=" + str(i*50)
-	response = urllib2.urlopen(url)
-	html = response.read()
+html = urllib2.urlopen("http://www.spoj.com/problems/").read()
+main_html = re.search(r'problems</a><br>(.*?)search', html, re.UNICODE | re.DOTALL)
 
-	REGEX = r'<tr class="problemrow">\n<td>(\d+)</td>\n.*\n<a href="/problems/(.*)/.*<b>(.*)</b>.*\n.*\n.*\n.*solutions.">(\d+).*\n.*problem.">([\d.]*)</a>'
-	pattern = re.compile(REGEX, re.UNICODE)
+categories_pattern = re.compile(r'"/problems/(.*?)"', re.UNICODE | re.DOTALL)
 
-	for match in pattern.finditer(html):
-		p = Problem()
-		p.judge = "spoj"
-		p.judge_id = "%s" % match.group(2)
-		p.name = "%s" % match.group(3)
-		p.url = "http://www.spoj.com/problems/%s/" % match.group(2)
-		p.users = "%s" % match.group(4)
-		p.accepted = "%s" % match.group(5)
-		p.dump()
+for category_match in categories_pattern.finditer(main_html.group(1)):
+	print "%s" % category_match.group(1)
+	page_url = "/problems/%s/" % category_match.group(1)
+	page_html = urllib2.urlopen("http://www.spoj.com" + str(page_url)).read()
+	
+	while(1):
+		#LOAD PROBLEMS
+		REGEX = r'problemrow.*?<a href="/problems/(.*?)/.*?<b>(.*?)<.*?ranks.*?>(\d+).*?status.*?>(.*?)<'
+		pattern = re.compile(REGEX, re.UNICODE | re.DOTALL)
+		for match in pattern.finditer(page_html):
+			p = Problem()
+			p.judge = "spoj"
+			p.judge_id = "%s" % match.group(1)
+			p.name = "%s" % match.group(2)
+			p.url = "http://www.spoj.com/problems/%s/" % match.group(1)
+			p.users = "%s" % match.group(3)
+			p.accepted = "%s" % match.group(4)
+			p.dump()
+		
+		more_page_html = re.search(r'<a href="' + re.escape(str(page_url)) + r'sort=(\d+),start=(\d+)" class="pager_link">Ne', 
+						page_html, re.UNICODE | re.DOTALL)
+		if more_page_html:
+			new_url = "sort=%s,start=%s" % (more_page_html.group(1), more_page_html.group(2))
+			page_html = urllib2.urlopen("http://www.spoj.com" + str(page_url) + str(new_url)).read()
+		else:
+			break
