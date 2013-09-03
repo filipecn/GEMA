6049ca2c2dc56733683f82d45002a9f1e61bbe6d
filipecn <fuiripecn@gmail.com>
2013-09-03 02:09:04 -0300

    update uva

--- alluva.py
+++ alluva.py
@@ -1,6 +1,8 @@
 import urllib2
 import urllib
-import re, utils
+import re
+
+from gemadb import Problem
 
 #PROBLEM SET VOLUMES
 
@@ -13,32 +15,18 @@ pattern = re.compile(REGEX, re.UNICODE)
 
 for match in pattern.finditer(html):
 	url = re.sub(r'&amp;', '&', "%s" % (match.group(1)))
+	print url
 	problems_html = urllib2.urlopen("http://uva.onlinejudge.org/%s" % url).read()
 
-	PROBLEM_REGEX = 'page=show_problem&amp'#;problem=(\d+)">(\d+)&nbsp;-&nbsp;(.*)</a></td>'
-	problem_pattern = re.compile(PROBLEM_REGEX, re.UNICODE)
-	print problems_html
-
-#	for problem_match in problem_pattern.finditer(problems_html):
-#		print "%s %s %s" % (problem_match.group(1), problem_match.group(2), problem_match.group(3))
-#						<td align="right">548754</td>
-#			<td>
-#			
-#
-#			
-#	<div style="width:150px;height:15px;background-color:#ff0000;position:relative;">
-#	<div style="width:27.31%;height:100%;background-color:#00ff00;"></div>
-#	<div style="width:100%;height:100%;text-align:center;position:absolute;top:-2px;left:0;font-size:13;font-family:Tahoma,Verdana,Arial,sans-serif;">27.31%</div>
-#	</div>
-#
-#
-#			
-#			</td>
-#
-#
-#									<td align="right">78437</td>
-#			<td>
-#	<div style="width:150px;height:15px;background-color:#ff0000;position:relative;">
-#	<div style="width:72.66%;height:100%;background-color:#00ff00;"></div>
-#	<div style="width:100%;height:100%;text-align:center;position:absolute;top:-2px;left:0;font-size:13;font-family:Tahoma,Verdana,Arial,sans-serif;">72.66%</div>
-#	</div>
+	PROBLEM_REGEX = 'page=show_problem&amp;problem=(\d+)">(\d+)&nbsp;-&nbsp;(.*?)<.*?>\d+.*?>.*?%<.*?(\d+).*?>.*?sans-serif;">(.*?)%'
+	problem_pattern = re.compile(PROBLEM_REGEX, re.UNICODE | re.DOTALL)
+	
+	for problem_match in problem_pattern.finditer(problems_html):
+		p = Problem()
+		p.judge = "uva"
+		p.judge_id = "%s" % problem_match.group(2)
+		p.name = "%s" % problem_match.group(3)
+		p.url = "http://uva.onlinejudge.org/%s&page=show_problem&problem=%s" % (url, problem_match.group(1))
+		p.users = "%s" % problem_match.group(4)
+		p.accepted = "%s" % problem_match.group(5)
+		p.dump()
