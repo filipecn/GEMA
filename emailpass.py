5c409e925885429f3f1cd60cda1f95348f7db085
filipecn <fuiripecn@gmail.com>
2013-09-03 16:43:24 -0300

    Update emailpass.py

--- emailpass.py
+++ emailpass.py
@@ -16,13 +16,21 @@ def sendemail(from_addr, to_addr_list, cc_addr_list,
     problems = server.sendmail(from_addr, to_addr_list, message)
     server.quit()
 
-#sendemail(from_addr    = 'fuiripecn@gmail.com', 
-#          to_addr_list = ['fuiripecn@gmail.com', 'biancamsoe@gmail.com', 'fabiolima39@gmail.com'],
-#          cc_addr_list = [], 
-#          subject      = 'Maestria no python', 
-#          message      = 'Inventei o python. Enviado pelo meu blackberry', 
-#          login        = 'fuiripecn', 
-#          password     = 'PASSWORD')
+f = open("usersregionais.txt", 'r').read()
 
-f = open("usersregionais.txt", 'r')
+import re
+
+pattern = re.compile(r'(.*?),(.*?),(.*?),(.*?),\n', re.UNICODE | re.DOTALL)
+
+for match in pattern.finditer(f):
+	print "%s %s %s %s" % (match.group(1),match.group(2),match.group(3),match.group(4))
+	msg = "Se vc esta recebendo este email, é pq deu certo meu script... aqui estão seu user e senha para os treinos do boca sobre a s provas regionais passadas..  user: %s  senha: %s" % (match.group(3),match.group(4)) 
+	sendemail(from_addr    = 'fuiripecn@gmail.com', 
+          to_addr_list = [str(match.group(1))],
+          cc_addr_list = [], 
+          subject      = 'User e senha maratona', 
+          message      = msg
+          login        = 'fuiripecn', 
+          password     = 'password')
+
 
