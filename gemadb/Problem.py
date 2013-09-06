0c2edfbe0d45ca60aad006046c84be7625ca990c
Rafael Regis <rafael.regisp@gmail.com>
2013-09-05 23:49:44 -0300

    Update gemadb lib.

---
+++ gemadb/Problem.py
@@ -0,0 +1,40 @@
+import json
+import urllib2
+
+
+class Problem:
+    def __init__(self, data):
+        self.problems_url = "http://gemadb-rrp.rhcloud.com/problems"
+        self.data = data
+
+    def save(self):
+        json_data = json.dumps(self.data)
+        length    = len(json_data)
+        req = urllib2.Request(problems_url, json_data,
+            {'Content-Type': 'application/json', 'Content-Length': length})
+        f = urllib2.urlopen(req)
+        status = f.getcode()
+        f.close()
+
+        return status == 200
+
+#Example
+if __name__ == "__main__":
+    data = {
+        'name':'test',
+        'url':'http://example.com',
+        'jid':'1234',
+        'total_submissions':-1,
+        'accepted_submissions':-1,
+        'total_users':-1,
+        'accepted_users':-1,
+        'description':'',
+        'spoiler':'',
+        'judge_id':'',
+        'tag_ids':[]
+    }
+    p = Problem(data)
+    if p.save():
+        print "SUCCESS!"
+    else:
+        print "FAIL!"
